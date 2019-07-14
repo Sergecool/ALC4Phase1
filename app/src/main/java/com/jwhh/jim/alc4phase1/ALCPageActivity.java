91853f25fa2c10519ba705a49ae731dcb568377d
jojo <sdjouno@yahoo.com>
2019-07-14 23:59:41 +0100

    modif

--- app/src/main/java/com/jwhh/jim/alc4phase1/ALCPageActivity.java
+++ app/src/main/java/com/jwhh/jim/alc4phase1/ALCPageActivity.java
@@ -4,7 +4,6 @@ import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.SslErrorHandler;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -16,14 +15,13 @@ public class ALCPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alcpage);
 
         WebView webView = findViewById(R.id.ebview_aplpage);
-        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
                 handler.proceed();
             }
         });
-        webSettings.setJavaScriptEnabled(true);
         webView.loadUrl("https://andela.com/alc");
     }
 }
